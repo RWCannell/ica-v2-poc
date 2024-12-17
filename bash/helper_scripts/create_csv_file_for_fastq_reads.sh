d11a365b91403004e68e81e4ac68634e8af36918
RWCannell <regancannell@gmail.com>
2024-12-17 12:08:47 +0200

    Updated params for sample ERR1019054

--- bash/helper_scripts/create_csv_file_for_fastq_reads.sh
+++ bash/helper_scripts/create_csv_file_for_fastq_reads.sh
@@ -3,9 +3,9 @@
 project_id="049307d6-85dd-4cdc-b88d-a740e4e9e550"
 time_stamp=$(date +"%Y-%m-%d %H:%M:%S")
 
-sample_id="ERR1019038"
-read_1_file_name="ERR1019038_1.fastq.gz"
-read_2_file_name="ERR1019038_2.fastq.gz"
+sample_id="ERR1019054"
+read_1_file_name="ERR1019054_1.fastq.gz"
+read_2_file_name="ERR1019054_2.fastq.gz"
 
 csv_file="fastq-list-$sample_id.csv"
 touch $csv_file
