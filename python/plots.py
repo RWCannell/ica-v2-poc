ae28c5e415028f2af3ca4cadb8c65e6c6722e317
RWCannell <regancannell@gmail.com>
2025-02-18 10:49:10 +0200

    Plotting various ICA data for SGDVP data

--- python/plots.py
+++ python/plots.py
@@ -3,12 +3,18 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-csv_file = "python/dragen_runtime_with_approximate_cost.csv"
+csv_file = "python/runtimes_and_costs_for_SGDVP_data.csv"
     
 df = pd.read_csv(csv_file)  
    
 dragen_runtime_values = df['dragen_runtime'].values
+workflow_runtime_values = df['workflow_runtime'].values
+approximate_cost_values = df['approximate_cost'].values
+actual_cost_values = df['actual_cost'].values
 sample_size_values = df['sample_size'].values
 approximate_cost_values = df['approximate_cost'].values
+
+plt.scatter(dragen_runtime_values, approximate_cost_values)
+plt.show()
 
 print(df.describe())
