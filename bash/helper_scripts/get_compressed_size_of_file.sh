060d03011fbe25a1feb15193b0a35f6e36ddbf7f
RWCannell <regancannell@gmail.com>
2025-03-24 22:47:47 +0200

    Added simple script for getting compressed file size

--- bash/helper_scripts/get_compressed_size_of_file.sh
+++ bash/helper_scripts/get_compressed_size_of_file.sh
@@ -3,4 +3,11 @@
 file_path="/dataA/1000G/SRR1295540_1.fastq.gz"
 
 compressed_file_size=$(gzip -t $file_path)
-echo $compressed_file_size
+
+if [ -z "$compressed_file_size" ]; then
+    echo "Compressed file size is: $compressed_file_size"
+    exit 0
+else
+    echo "Could not get compressed file size: exiting with error."
+    exit 1
+fi
