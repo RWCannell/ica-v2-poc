b85a7238076c3239a65332ebca2190fc4309abd6
RWCannell <regancannell@gmail.com>
2025-05-15 09:32:09 +0200

    Modified upload script - need to test

--- bash/helper_scripts/upload_pair_of_files.sh
+++ bash/helper_scripts/upload_pair_of_files.sh
@@ -2,17 +2,14 @@
 
 file_path="$HOME/Documents/RWCannell/ica-v2-poc/public/assets/ica_data_uploads/fastq/1_control_rbcLa_2019_minq7"
 project_id="049307d6-85dd-4cdc-b88d-a740e4e9e550"
+sample_id=""
 time_stamp=$(date +"%Y-%m-%d %H:%M:%S")
 
-output_file_prefix=$(echo "${file_path##*/}")
+output_file_prefix=$sample_id
 read_1_file=""
 read_2_file=""
 read_1_analysis_code="read1"
 read_2_analysis_code="read2"
-ica_path=$(echo "/${file_path##*/}/")
-
-manifest_file="manifest.tsv"
-touch $manifest_file
 
 for file in "$file_path"/*; do
     if [[ "$file" == *"_R1_"* ]]; then
@@ -34,12 +31,12 @@ touch $read_2_file_response
 
 printf "[$time_stamp]: "
 printf "Uploading read 1 file '$read_1_file'... \n"
-read_1_upload_response=$(icav2 projectdata upload $read_1_file $ica_path --project-id $project_id)
+read_1_upload_response=$(icav2 projectdata upload $read_1_file --project-id $project_id)
 echo "$read_1_upload_response" > $read_1_file_response
 
 printf "[$time_stamp]: "
 printf "Uploading read 2 file '$read_2_file'... \n"
-read_2_upload_response=$(icav2 projectdata upload $read_2_file $ica_path --project-id $project_id)
+read_2_upload_response=$(icav2 projectdata upload $read_2_file --project-id $project_id)
 echo "$read_2_upload_response" > $read_2_file_response
 
 # id of file starts with 'fil.'
@@ -47,20 +44,23 @@ read_1_file_id=$(grep -i "\"id\": \"fil\." $read_1_file_response | grep -o 'fil[
 read_2_file_id=$(grep -i "\"id\": \"fil\." $read_2_file_response | grep -o 'fil[^\"]*')
 
 printf "[$time_stamp]: "
-printf "Writing read file ids to manifest file...\n"
+printf "Writing read file ids to data file...\n"
 
-manifest_header_1="file_name"
-manifest_header_2="file_id"
-manifest_header_3="file_ref"
-manifest_tab=$(printf "\t")
-read_1_file_ref="$read_1_analysis_code:$read_1_file_id"
-read_2_file_ref="$read_2_analysis_code:$read_2_file_id"
+data_file="data.txt"
 
-cat > $manifest_file << EOF
-$manifest_header_1 $manifest_tab $manifest_header_2 $manifest_tab $manifest_header_3
-$(echo "${read_1_file##*/}") $manifest_tab $read_1_file_id $manifest_tab $read_1_file_ref
-$(echo "${read_2_file##*/}") $manifest_tab $read_2_file_id $manifest_tab $read_2_file_ref
-EOF
+if ! [ -f ${data_file} ]; then
+    echo "Data file does not exist. Creating one..."
+    touch ${data_file}
+fi
+
+printf "[${time_stamp}]: "
+printf "Writing file data to existing data file...\n"
+
+printf "sampleId:${sampleId}\n" >> ${data_file}
+printf "${read1AnalysisDataCode}:${read_1_file_id}\n" >> ${data_file}
+printf "${read2AnalysisDataCode}:${read_2_file_id}\n" >> ${data_file}
+printf "read1Name:\${read_1_uploaded_file_name}\n" >> ${data_file}
+printf "read2Name:\${read_2_uploaded_file_name}\n" >> ${data_file}
 
 printf "[$time_stamp]: "
 printf "Removing temporary files...\n"
