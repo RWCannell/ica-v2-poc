b7235ceac5425ade672e564b33dc58da2628dd1c
RWCannell <regancannell@gmail.com>
2025-05-15 10:11:46 +0200

    Fixed syntax errors

--- bash/helper_scripts/start_nextflow_dragen_analysis.sh
+++ bash/helper_scripts/start_nextflow_dragen_analysis.sh
@@ -4,16 +4,16 @@ project_id="049307d6-85dd-4cdc-b88d-a740e4e9e550"
 pipeline_id="858708e9-6cdd-42ae-ad97-8b86ee07cdc5"
 user_reference="dragen_germline_whole_genome"
 storage_size="Medium"
-$read_1_analysis_code="read1"
-$read_2_analysis_code="read2"
-$fastqs_analysis_data_code="fastqs"
-$fastq_list_data_code="fastq_list",
-$ref_file_analysis_code"ref_tar"
+read_1_analysis_code="read1"
+read_2_analysis_code="read2"
+fastqs_analysis_data_code="fastqs"
+fastq_list_data_code="fastq_list",
+ref_file_analysis_code="ref_tar"
 
-$ref_file_id="fil.2ed2474579454e9ff42d08dc4483ed72"
-$read_1_file_id="fil.5068092a48584ae4d94b08dd8ca25b14"
-$read_2_file_id="fil.a93d08fe0a9443a1d94c08dd8ca25b14"
-$fastq_list_file_id="fil.f2106d05358849299b0a08dd8c9b02f1"
+ref_file_id="fil.2ed2474579454e9ff42d08dc4483ed72"
+read_1_file_id="fil.5068092a48584ae4d94b08dd8ca25b14"
+read_2_file_id="fil.a93d08fe0a9443a1d94c08dd8ca25b14"
+fastq_list_file_id="fil.f2106d05358849299b0a08dd8c9b02f1"
 
 analysis_response=$(icav2 projectpipelines start nextflow $pipeline_id \
     --user-reference $user_reference \
