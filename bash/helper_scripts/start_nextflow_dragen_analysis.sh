465b1cd252272b4b70c60b0f441b2ea2f6411790
RWCannell <regancannell@gmail.com>
2025-05-15 10:07:32 +0200

    Testing the running of a DRAGEN analysis

--- bash/helper_scripts/start_nextflow_dragen_analysis.sh
+++ bash/helper_scripts/start_nextflow_dragen_analysis.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
 
 project_id="049307d6-85dd-4cdc-b88d-a740e4e9e550"
-pipeline_code="basic_pipeline"
-pipeline_id="bfecca03-6443-45bd-b313-e4f555cd0748"
-user_reference="regan_dragen_analysis_01"
-storage_size="Small"
-file_name="NZ_GG704940.fa"
-file_id="fil.09b47f75e6014039ac4908dc6dfe2358"
+pipeline_id="858708e9-6cdd-42ae-ad97-8b86ee07cdc5"
+user_reference="dragen_germline_whole_genome"
+storage_size="Medium"
 $read_1_analysis_code="read1"
-$read_2_analysis_code"read2"
+$read_2_analysis_code="read2"
+$fastqs_analysis_data_code="fastqs"
+$fastq_list_data_code="fastq_list",
 $ref_file_analysis_code"ref_tar"
-output_file_directory="/NZ_GG704940/output_files/"
-output_file_prefix="NZ_GG704940"
 
-icav2 projectpipelines start nextflow $pipeline_id \
-    --user-reference $user_reference \
-    --project-id $project_id \
-    --storage-size $storage_size \
-    --input "$read_1_analysis_code:$read_1_file_id" \
-    --input "$read_2_analysis_code:$read_2_file_id" \
-    --input "$ref_file_analysis_code:$ref_file_id" \
-    --parameters enable-variant-caller:true \
-    --parameters RGID:Illumina_RGID \
-    --parameters RGSM:$output_file_prefix \
-    --parameters output-directory:$output_file_directory \
-    --parameters output-file-prefix:$output_file_prefix 
+$ref_file_id="fil.2ed2474579454e9ff42d08dc4483ed72"
+$read_1_file_id="fil.5068092a48584ae4d94b08dd8ca25b14"
+$read_2_file_id="fil.a93d08fe0a9443a1d94c08dd8ca25b14"
+$fastq_list_file_id="fil.f2106d05358849299b0a08dd8c9b02f1"
+
+analysis_response=$(icav2 projectpipelines start nextflow ${pipeline_id} \
+    --user-reference ${user_reference} \
+    --project-id ${project_id} \
+    --storage-size ${storage_size} \
+    --input ${ref_file_analysis_code}:"$ref_file_id" \
+    --input ${fastqs_analysis_data_code}:"${read_1_file_id},${read_2_file_id}" \
+    --input ${fastq_list_data_code}:${fastq_list_file_id} \
+    --parameters enable_map_align:true \
+    --parameters enable_map_align_output:true \
+    --parameters output_format:CRAM \
+    --parameters enable_duplicate_marking:true \
+    --parameters enable_variant_caller:true \
+    --parameters vc_emit_ref_confidence:GVCF \
+    --parameters vc_enable_vcf_output:true \
+    --parameters enable_cnv:true \
+    --parameters enable_sv:true \
+    --parameters repeat_genotype_enable:false \
+    --parameters enable_hla:false \
+    --parameters enable_variant_annotation:false \
+    --parameters output_file_prefix:"${sample_id}")
+
+$analysis_response_file="analysis_response.txt"
+touch ${analysis_response_file}
+echo "${analysis_response}" > ${analysis_response_file}
